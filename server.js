e8a17709ce9998be90cda4d289a7789535bcae6b
Advitya sharma <54709416+Advitya-sharma@users.noreply.github.com>
2020-07-25 08:41:50 +0530

    completing json response

--- server.js
+++ server.js
@@ -1,21 +1,37 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var cors = require('cors');
-
-var app = express();
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get("/hello", function (req, res) {
+  res.json({ greetings: "Hello, API" });
+});
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const file = req.file;
+  res.json({ name: file.originalname, type: file.mimetype, size: file.size });
 });
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+  console.log("Node.js listening ...");
 });
